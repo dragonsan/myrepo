f56396c17b1f28fa1a54ddc228b713b7da577cbc
Justin Baltazar <biasa199@Justins-MacBook-Air.local>
2013-07-13 00:12:23 -0500

    ohaidere

--- public/index.php
+++ public/index.php
@@ -4,79 +4,29 @@ A summer project by Justin Baltazar (2013).
 Name: Empi
 Creator: Justin B.
 Description: To be able to identify malicious threats and act accordingly.
+Comments: Dedicated to Sabrina.
 */
-setcookie("verify", "1", time()+300);  /* expire in 1 hour */
-if (!isset($_SESSION['verify'])) {
-	$_SESSION['verify']="1";
-}
-if (isset($_SESSION['verify'])){
-	if ($_SESSION['verify'] == $_COOKIE['verify']) {
-
-	}
-	else {
-		die('HBA check failed.');
-	}
-}
-error_reporting(0);
-$canswer = $_COOKIE['answer'];
-if (!isset($_SESSION)) {
-        session_start();
-}
-$_SESSION['verify']="Hello"; 
-if(isset($_SESSION['answer']))
-{
-	if ($canswer == $_SESSION['answer']){
-		$_SESSION['answer'] = '0';
-		unset($_SESSION['answer']);
-		goto alpha;
-	}
+begin:
+session_start();
+set_time_limit(5);
+$uri = md5($_SERVER['REQUEST_URI']);
+$exp = 3; //Seconds before expiry.
+$hash = $uri .'|'. time();
+if (!isset($_SESSION['ddos'])) {
+    $_SESSION['ddos'] = $hash;
 }
 
-if($_SESSION['last_session_request'] > time() - 2){
-        // Do Human check here
-		$first = rand(0,10) / 10;
-		$n1 = $first * rand(0,10); // 87
-		$n2 = $first * rand(0,10); // 87
-		$n3 = $first * rand(0,10); // 87
-		$answer = $n1 + $n2 * $n3;
-		$_SESSION['answer']=$answer;
-		goto end;
-}
-if (isset($_SESSION['verify'])) {
-$_SESSION['last_session_request'] = time();
-$z = file_get_contents("http://138.91.76.107/lol.php");
-die($z);
-}
-else {
-	die('HBA check failed.');
-} 
 
-alpha:
+list($_uri, $_exp) = explode('|', $_SESSION['ddos']);
+if ($_uri == $uri && time() - $_exp < $exp) {
+	sleep (3000);
+	goto begin;
+	//echo '<script>location.reload(true);</script><noscript><META HTTP-EQUIV="refresh" CONTENT="15">Loading....</noscript>';
+    die;
+}
+
+// Save last request
 $z = file_get_contents("http://138.91.76.107/lol.php");
 echo $z;
-exit;
-
-end:
-if (!isset($_SESSION['verify'])) {
-	die('HBA check failed.');
-}
-echo <<<END
-<script type="text/javascript">
-function createCookie(name,value,days) {
-	if (days) {
-	        var date = new Date();
-	        date.setTime(date.getTime()+(days*60*1000));
-	        var expires = "; expires="+date.toGMTString();
-	}
-	    else var expires = "";
-	    document.cookie = name+"="+value+expires+"; path=/";
-	}
-answer = $n1 + $n2 * $n3
-createCookie ('answer', answer, '5');
-location.reload(true);
-</script>
-END;
-exit;
+$_SESSION['ddos'] = $hash;
 ?>
-
-
